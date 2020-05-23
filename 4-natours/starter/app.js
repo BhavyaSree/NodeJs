bef4d088b401054a276dbdc65e92a204da12fd82
Bhavya Bindela <bbindela@hawk.iit.edu>
2020-05-23 09:45:41 -0500

    environment variables&Eslint with prettier

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -7,7 +7,9 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MIDDLEWARES
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
 app.use(express.json());
 
 // To access static files
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,14 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+// console.log(app.get('env'));   // to get in which environment we are running
+
+// console.log(process.env);
+
 // SERVER
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
-
