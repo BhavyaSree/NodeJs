572af27ddd715b2fee522e15d9ea42259ac6a2b2
Bhavya Bindela <bbindela@hawk.iit.edu>
2020-06-01 22:45:36 -0500

    MongoDB with Mongoose - Document, query, aggregate middlewares. data validators

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -70,7 +70,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: err,
     });
   }
 };
